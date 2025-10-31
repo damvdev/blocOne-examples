3f2d37b0b5d5f717783ec7f55f50858d30e8d7e0
Raquel Alamán <raquel.alaman@itb.cat>
2025-10-31 09:25:15 +0100

    Added formated strings examples

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Examples {
+    public class Program
+    {
+        public static void Main()
+        {
+            const string Message = "Hello, my name is {0} and I am {1} years old.";
+            int age = 30;
+            string name = "Alice";
+
+            /****** Interpolated strings example ******/
+            string greeting = $"Hello, my name is {name} and I am {age} years old.";
+
+            Console.WriteLine(greeting);
+            //or
+            Console.WriteLine($"Hello, my name is {name} and I am {age} years old.");
+
+            /****** Composite formatting strings example ******/
+            Console.WriteLine(Message, name, age);
+
+        }
+    }
+}
